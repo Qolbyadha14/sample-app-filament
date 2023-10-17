d358741e877ce08221999fac2e7162778f696845
qolby.adha <qolby.adha@kawanlamacorp.com>
2023-10-17 17:18:16 +0700

    custom logo header dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Poppins')
             ->darkMode(true)
+            //->favicon()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,5 @@
+<img
+    src="{{asset('images/logo-prostasia-removebg-preview.png')}}"
+    alt="logo prostasia"
+    class="h-16"
+/>
